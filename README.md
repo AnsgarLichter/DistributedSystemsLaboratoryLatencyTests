bf2feb9511dcb647226bc5b6c9997e1c9ddace1d
Ansgar Lichter <LichterAnsgar@gmail.com>
2023-06-06 16:57:37 +0200

    docs(readme): add alternate command

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Execute the following command to start the simulation:
 ```bash
 mvn gatling:test -Dgatling.simulationClass=webshop.WebshopSimulation
 ```
+
+If the command doesn't work, you can try the following version:
+
+```bash
+mvn gatling:test -D"gatling.simulationClass"="webshop.WebshopSimulation"
+```
--- src/test/java/webshop/WebshopSimulation.java
+++ src/test/java/webshop/WebshopSimulation.java
@@ -114,13 +114,13 @@ public class WebshopSimulation extends Simulation {
 
     {
         setUp(
-                createCategoriesScenario.injectOpen(rampUsers(50).during(10)),
+                createCategoriesScenario.injectOpen(rampUsers(1).during(10)),
 
-                createProductsScenario.injectOpen(rampUsers(100).during(10)),
-                getProductsScenario.injectOpen(rampUsers(100).during(10)),
-                deleteProductsScenario.injectOpen(rampUsers(100).during(10)),
+                createProductsScenario.injectOpen(rampUsers(1).during(10)),
+                getProductsScenario.injectOpen(rampUsers(1).during(10)),
+                deleteProductsScenario.injectOpen(rampUsers(1).during(10)),
 
-                deleteCategoriesScenario.injectOpen(rampUsers(50).during(10))
+                deleteCategoriesScenario.injectOpen(rampUsers(1).during(10))
                 ).protocols(httpProtocol);
     }
 
